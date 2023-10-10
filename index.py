3fe579c5551b06ee281286a7b180d46d5f57b6b6
hebx <hebamaher0101@gmail.com>
2023-10-11 00:39:52 +0300

    Update index.py

--- index.py
+++ index.py
@@ -1,14 +1,29 @@
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
 
-bot=ChatBot("chatbot",read_only=False,logic_adapters=["chatterbot.logic.BestMatch"])
+bot0=ChatBot("chatbot",read_only=False,logic_adapters=["chatterbot.logic.BestMatch"])
 
 list_to_train=[
- """,
- """,
+ "hi",
+ "hey there!",
+ "what's your name ",
+ "I'm vera",
+ "how old are you?",
+ "I am a chatbot and do not have an age. However I was created by Heba Maher in october,2023",
+ "what can you do?",
+ "I am a simple general purpose chatbot that can tell jokes as well",
+ "tell me a joke",
+ "Where do fish keep their money?   In a river bank. "
 
+ 
 ]
 
-list_trainer =ListTrainer(bot)
+list_trainer =ListTrainer(bot0)
 
-list_trainer.train()
+list_trainer.train(list_to_train)
+print("what is your name? ")
+user_name =input()
+
+while True:
+  user_response=input(user_name+": ")
+  print("vera: "+ str(bot0.get_response(user_response)))
