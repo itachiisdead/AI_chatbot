7e30de0524ce838f1835386bd369df21de288917
hebx <hebamaher0101@gmail.com>
2023-10-14 01:15:15 +0300

    added premade datasets for conversations

--- convo.py
+++ convo.py
@@ -1,6 +1,8 @@
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer, ChatterBotCorpusTrainer
+from flask import Flask,render_template
 
+app=Flask(__name__)
 
 beepbop=ChatBot("chatbot",read_only=False,
              logic_adapters=[
@@ -13,29 +15,33 @@ beepbop=ChatBot("chatbot",read_only=False,
 
 
 list_to_train=[
- "hi",
- "hey there!",
  "what's your name ",
  "I'm BeepBop",
  "how old are you?",
- "I am a chatbot and do not have an age. However I was created by Heba Maher in october,2023",
- "what can you do?",
- "I am a simple general purpose chatbot that can tell jokes as well",
+ "I do not have an age. However I was created by Heba Maher in october,2023",
  "who made you?",
  "Heba Maher created me in october,2023",
- 
 ]
-
-list_trainer =ListTrainer(beepbop)
-list_trainer.train(list_to_train)
 
 trainer = ChatterBotCorpusTrainer(beepbop)
 trainer.train("chatterbot.corpus.english")
 
+list_trainer =ListTrainer(beepbop)
+list_trainer.train(list_to_train)
 
 print("what is your name? ")
 user_name =input()
 
+@app.route("/")
+def main():
+  return render_template("index.html")
+
+
+
 while True:
   user_response=input(user_name+": ")
-  print("BeepBop: "+ str(beepbop.get_response(user_response))) 
+  print("BeepBop: "+ str(beepbop.get_response(user_response)))
+
+
+  if __name__ =="__main__":
+    app.run(depug=True) 
--- index.py
+++ index.py
@@ -1,29 +1,33 @@
 from chatterbot import ChatBot
-from chatterbot.trainers import ListTrainer
+from chatterbot.trainers import ListTrainer, ChatterBotCorpusTrainer
+from flask import Flask,render_template
 
-bot0=ChatBot("chatbot",read_only=False,logic_adapters=["chatterbot.logic.BestMatch"])
+app=Flask(__name__)
 
-list_to_train=[
- "hi",
- "hey there!",
- "what's your name ",
- "I'm vera",
- "how old are you?",
- "I am a chatbot and do not have an age. However I was created by Heba Maher in october,2023",
- "what can you do?",
- "I am a simple general purpose chatbot that can tell jokes as well",
- "tell me a joke",
- "Where do fish keep their money?   In a river bank. "
+beepbop=ChatBot("chatbot",read_only=False,
+             logic_adapters=[
+               {
+               "import_path":"chatterbot.logic.BestMatch",
+               "default_response":"sorry I don't have an answer",
+               "maximum_similarity_threshold":0.9
+               }
+               ])
 
- 
-]
 
-list_trainer =ListTrainer(bot0)
+trainer = ChatterBotCorpusTrainer(beepbop)
 
-list_trainer.train(list_to_train)
-print("what is your name? ")
-user_name =input()
+#trainer.train("chatterbot.corpus.english")
 
-while True:
-  user_response=input(user_name+": ")
-  print("vera: "+ str(bot0.get_response(user_response)))
+@app.route("/")
+def main():
+   return render_template("index.html")
+
+
+
+#while True:
+ # user_response=input("User: ")
+  #print("BeepBop: "+ str(beepbop.get_response(user_response)))
+
+
+if __name__ =="__main__":
+     app.run(debug=True) 
