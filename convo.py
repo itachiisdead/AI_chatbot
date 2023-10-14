8aa9b14e9a1fa53c23b48ff6ddad988b94df25f1
hebx <hebamaher0101@gmail.com>
2023-10-15 00:21:22 +0300

    Update convo.py

--- convo.py
+++ convo.py
@@ -1,8 +1,35 @@
 from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+
+bot0=ChatBot("chatbot",read_only=False,logic_adapters=["chatterbot.logic.BestMatch"])
+
+list_to_train=[
+ "hi",
+ "hey there!",
+ "what's your name ",
+ "I'm vera",
+ "how old are you?",
+ "I am a chatbot and do not have an age. However I was created by Heba Maher in october,2023",
+ "what can you do?",
+ "I am a simple general purpose chatbot that can tell jokes as well",
+ "tell me a joke",
+ "Where do fish keep their money?   In a river bank. "
+
+]
+
+list_trainer =ListTrainer(bot0)
+
+list_trainer.train(list_to_train)
+print("what is your name? ")
+user_name =input()
+
+while True:
+  user_response=input(user_name+": ")
+  print("vera: "+ str(bot0.get_response(user_response)))
 from chatterbot.trainers import ListTrainer, ChatterBotCorpusTrainer
 from flask import Flask,render_template
 
-app=Flask(__name__)
+app=Flask(_name_)
 
 beepbop=ChatBot("chatbot",read_only=False,
              logic_adapters=[
@@ -14,23 +41,9 @@ beepbop=ChatBot("chatbot",read_only=False,
                ])
 
 
-list_to_train=[
- "what's your name ",
- "I'm BeepBop",
- "how old are you?",
- "I do not have an age. However I was created by Heba Maher in october,2023",
- "who made you?",
- "Heba Maher created me in october,2023",
-]
-
 trainer = ChatterBotCorpusTrainer(beepbop)
-trainer.train("chatterbot.corpus.english")
 
-list_trainer =ListTrainer(beepbop)
-list_trainer.train(list_to_train)
-
-print("what is your name? ")
-user_name =input()
+#trainer.train("chatterbot.corpus.english")
 
 @app.route("/")
 def main():
@@ -38,10 +51,10 @@ def main():
 
 
 
-while True:
-  user_response=input(user_name+": ")
-  print("BeepBop: "+ str(beepbop.get_response(user_response)))
+#while True:
+ # user_response=input("User: ")
+  #print("BeepBop: "+ str(beepbop.get_response(user_response)))
 
 
-  if __name__ =="__main__":
-    app.run(depug=True) 
+  if _name_ =="_main_":
+    app.run(depug=True)
