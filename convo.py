33e15953d94900729b0396b9b988ea8e3e81ce84
hebx <hebamaher0101@gmail.com>
2023-10-16 12:59:01 +0300

    Update convo.py

--- convo.py
+++ convo.py
@@ -13,9 +13,11 @@ list_to_train=[
  "what can you do?",
  "I am a simple general purpose chatbot that can tell jokes as well",
  "tell me a joke",
- "Where do fish keep their money?   In a river bank. "
+ "Where do fish keep their money?   In a river bank. ",
 
-]
+
+] 
+
 
 list_trainer =ListTrainer(bot0)
 
@@ -26,35 +28,5 @@ user_name =input()
 while True:
   user_response=input(user_name+": ")
   print("vera: "+ str(bot0.get_response(user_response)))
-from chatterbot.trainers import ListTrainer, ChatterBotCorpusTrainer
-from flask import Flask,render_template
-
-app=Flask(_name_)
-
-beepbop=ChatBot("chatbot",read_only=False,
-             logic_adapters=[
-               {
-               "import_path":"chatterbot.logic.BestMatch",
-               "default_response":"sorry I don't have an answer",
-               "maximum_similarity_threshold":0.9
-               }
-               ])
 
 
-trainer = ChatterBotCorpusTrainer(beepbop)
-
-#trainer.train("chatterbot.corpus.english")
-
-@app.route("/")
-def main():
-  return render_template("index.html")
-
-
-
-#while True:
- # user_response=input("User: ")
-  #print("BeepBop: "+ str(beepbop.get_response(user_response)))
-
-
-  if _name_ =="_main_":
-    app.run(depug=True)
