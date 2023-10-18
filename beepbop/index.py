a7fecac3b7e35098916cb3dd2a6fe7059c91d72a
hebx <hebamaher0101@gmail.com>
2023-10-18 22:34:31 +0300

    added new chatbot

--- beepbop/index.py
+++ beepbop/index.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
-
+import requests 
 app = Flask(__name__)
 
 # Create a ChatBot instance
@@ -21,7 +21,9 @@ def index():
 def get_chatbot_response():
     user_message = request.form["user_message"]
     response = chatbot.get_response(user_message)
-    return str(response)
+    rawdata=requests.get("https://api.openweathermap.org/data/2.5/weather?q=cairo&appid=8a5f0459dbfb397e81ac7d506b5e648f")
+    result=rawdata.json()
+    return str(result)
 
 # Run the Flask application
 if __name__ == "__main__":
--- units.py
+++
@@ -1,9 +0,0 @@
-#this is a chatbot that can convert units
-from chatterbot import ChatBot
-
-bot=ChatBot("units",logic_adapters=["chatterbot.logic.UnitConversion"])
-
-while True:
-    user_text=input("use unit conversion: ")
-    ChatBot_response = str(bot.get_response(user_text))
-    print(ChatBot_response )
